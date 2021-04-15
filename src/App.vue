57b67b6ee9f1deb548cf347ac9d02ef08cea70ef
Nordy Vlasman <nordyvla@gmail.com>
2021-04-15 23:46:11 +0200

    Improved authentication flow:

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <button @click="logout">Log out</button>
   </div>
   <router-view/>
 </template>
@@ -9,10 +10,24 @@
 import { defineComponent, provide } from 'vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import apolloClient from './graphql/client'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   setup () {
     provide(DefaultApolloClient, apolloClient)
+
+    const router = useRouter()
+
+    const logout = () => {
+      localStorage.removeItem('authentication_token')
+      router.push({
+        name: 'Login'
+      })
+    }
+
+    return {
+      logout
+    }
   }
 })
 </script>
