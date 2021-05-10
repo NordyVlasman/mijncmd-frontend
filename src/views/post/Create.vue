abc9188a82a52592146e93c0fdd8abd14ecb9e7d
Nordy Vlasman <nordy@sigbar.nl>
2021-05-10 11:43:20 +0200

    Changes

--- src/views/post/Create.vue
+++ src/views/post/Create.vue
@@ -1,8 +1,91 @@
 <template>
-  <div>
-    <div class="container bg-white">
-      <div id="editor-container" class="prose lg:prose-xl"/>
+    <main class="relative flex-1 overflow-y-auto focus:outline-none">
+      <div class="px-6 py-6">
+      <form @submit.prevent='createPost'>
+  <div class="space-y-6">
+    <div class="px-4 py-5 bg-white shadow sm:rounded-lg sm:p-6">
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">Post beschrijving</h3>
+          <p class="mt-1 text-sm text-gray-500">
+            This information will be displayed publicly so be careful what you share.
+          </p>
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <form class="space-y-6" action="#" method="POST">
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="title" class="block text-sm font-medium text-gray-700">
+                  Titel
+                </label>
+                <div class="flex mt-1 rounded-md shadow-sm">
+                    <input type="text" name="Title" id="title" v-model="form.title"  class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"  />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label for="description" class="block text-sm font-medium text-gray-700">
+                Beschrijving
+              </label>
+              <div class="mt-1">
+                <textarea id="description" name="description" rows="3" v-model="form.description"  class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+              </div>
+            </div>
+
+            <div>
+              <label class="block text-sm font-medium text-gray-700">
+                Cover photo
+              </label>
+              <div class="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
+                <div class="space-y-1 text-center">
+                  <svg class="w-12 h-12 mx-auto text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  <div class="flex text-sm text-gray-600">
+                    <label for="file-upload" class="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                  </div>
+                  <p class="text-xs text-gray-500">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+
+    <div class="px-4 py-5 bg-white shadow sm:rounded-lg sm:p-6">
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">Content ✨</h3>
+          <p class="mt-1 text-sm text-gray-500">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+            <div id="editor-container" class="bg-gray-200">
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-end">
+      <button type="button" class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Cancel
+      </button>
+      <button type="submit" class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Save
+      </button>
+    </div>
+  </div>
+    </form>
+
 <!-- <div>
     <div class="md:grid md:grid-cols-3 md:gap-6">
       <div class="md:col-span-1">
@@ -100,6 +183,7 @@
     </div>
   </div> -->
   </div>
+          </main>
 </template>
 <script>
 import EditorJS from '@editorjs/editorjs'
@@ -150,9 +234,15 @@ export default {
     },
     async createPost () {
       const payload = this.form
-      payload.slug = this.slug
-      console.log(payload)
-      this.$store.dispatch('posts/createPost', payload)
+      editor.save().then(savedData => {
+        payload.body = JSON.stringify(savedData)
+        payload.slug = this.slug
+        console.log(payload)
+        this.$store.dispatch('posts/createPost', payload)
+      })
+      // payload.slug = this.slug
+      // console.log(payload)
+      // this.$store.dispatch('posts/createPost', payload)
     }
   },
   mounted () {
@@ -162,12 +252,8 @@ export default {
 </script>
 
 <style>
-editorx_body {
-  /* width: 62%;
-  margin-left: 15%; */
-  width: 60%;
-  margin-left: 20%;
-  border: 2px solid #f1f3f5;
-  box-sizing: border-box;
+.ce-header {
+  font-size: revert;
+  font-weight: revert;
 }
 </style>
--- src/views/post/{slug}.vue
+++ src/views/post/{slug}.vue
@@ -7,10 +7,13 @@
       </span>
     </div>
     <p class="font-medium">{{ selectedPost.description }}</p>
-    <markdown-renderer
+    <!-- <markdown-renderer
       class="max-w-full prose"
       :markdown="selectedPost.body"
-    />
+    /> -->
+    <div v-for="block in body.blocks" :key="block.id">
+      {{ block }}
+    </div>
   </div>
 </template>
 <script>
@@ -25,11 +28,13 @@ export default {
   ],
   data () {
     return {
-      selectedPost: null
+      selectedPost: null,
+      body: null
     }
   },
   created () {
     this.selectedPost = this.$store.getters['posts/posts'].find((post) => post.id === this.postId)
+    this.body = JSON.parse(this.selectedPost.body)
   }
 }
 </script>
